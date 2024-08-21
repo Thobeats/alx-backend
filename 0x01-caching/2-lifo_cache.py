7bca9cedb4efd54c6619126c80d871620bc939ec
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-22 00:11:18 +0100

    lifo cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" LIFOCache Module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache Class
+    """
+
+    def put(self, key, item) -> None:
+        """put
+
+        Keyword arguments:
+        key -- key value to be store in the cache
+        item -- value to be store in cache
+        Return: None
+        """
+        if key and item:
+            if len(self.cache_data) == self.MAX_ITEMS:
+                keys = list(self.cache_data.keys())
+                del self.cache_data[keys[-1]]
+                print("DISCARD: {}".format(keys[-1]))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get
+
+        Keyword arguments:
+        key -- the key value to retrieve a data from the cache
+        Return: the stored value or None if the key does not exist
+        """
+        if key and key in self.cache_data:
+            return self.cache_data.get(key)
+        return None
