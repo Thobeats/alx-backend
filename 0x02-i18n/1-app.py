41cdecb981c2e9e9557c37f269bbf1d96109453e
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-28 01:52:33 +0100

    configs

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,7 +11,7 @@ class Config(object):
     """
     Config Variables
     """
-    LANGUAGES = ['en', 'fr']
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
