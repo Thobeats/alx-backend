689da054b8658c8e049228ac4048186811f3ca13
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-28 02:59:17 +0100

    translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config(object):
+    """
+    Config Variables
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
